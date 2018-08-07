58a85442199093d9f0e222692ab49932e0609ffb
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-07 12:19:28 -0700

    booleanComparison
    while and do while in program.cs

--- booleanComparison/Program.cs
+++ booleanComparison/Program.cs
@@ -10,6 +10,47 @@ namespace booleanComparison
     {
         static void Main(string[] args)
         {
+
+            //While Statement
+            
+            Console.WriteLine("Pick a whole number and I will count it down to 0.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                if (-1 < number)
+                {
+                    Console.WriteLine(number);
+                    number--;
+                }
+            }
+            Console.ReadLine();
+
+            
+            //Do While Statement
+
+            Console.WriteLine("Pick a whole number and I will tell you if it is Even or Odd");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int remainder = num % 2;
+            do
+            {
+                if (remainder == 1)
+                {
+                    Console.WriteLine("The number " + num + " is Odd.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("The number " + num + " is Even.");
+                    break;
+                }
+            }
+            while (true);
+
+            Console.ReadLine();
+
+
+            
+       
         }
     }
 }
